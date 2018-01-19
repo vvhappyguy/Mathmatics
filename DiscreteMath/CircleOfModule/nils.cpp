176f86a6e69cbc7fe84f809c1398a8de37c14d2b
Ivan <dyakin_ip@outlook.com>
2018-01-20 01:34:30 +0300

    Better UI in nils program.

--- DiscreteMath/CircleOfModule/nils.cpp
+++ DiscreteMath/CircleOfModule/nils.cpp
@@ -4,16 +4,26 @@
 using namespace std;
 
 int main(){
+    cout << "Write module of Circle: ";
     int counter = 0;
-    int z = 44;
+    int z;
+    int max = 0;
     cin >> z;
     for(int i = 1; i < z; i++){
         for(int j = 2; j < z; j++){
             if((int)pow(i,j) % z == 0){
                 cout << i << " "<< j<<endl;
                 counter++;
+                max = j;
             } 
         }
     }
-    cout << endl << counter << endl;
+    if(max != 0){
+        cout << "Max order of element is " << max << endl;
+    } else {
+        cout << "There are no nils" << endl;
+    }
+    if(counter > 1){
+        cout << "Count of nils: " << counter << endl;
+    }
 }
