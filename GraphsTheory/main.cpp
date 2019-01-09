6acc79888b1a4127dd6bab653603664c91d47efa
Ivan Dyakin <vvhappyguy@gmail.com>
2019-01-09 12:34:57 +0300

    Adjacency matrix added

--- GraphsTheory/main.cpp
+++ GraphsTheory/main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <set>
-#include <stack>
-#include <vector>
+#include <iostream> // IO
+#include <vector>   // Connected nodes
+#include <set>      // For DFS and BFS
+#include <stack>    // DFS
+#include <queue>    // BFS
 
 using namespace std;
 
@@ -18,7 +19,7 @@ class Node
         this->number = counter++;
         this->value = _value;
         this->connected_nodes.swap(_connected_nodes);
-        for (Node* node: connected_nodes)
+        for (Node *node : connected_nodes)
             node->connected_nodes.push_back(this);
     };
 
@@ -26,6 +27,49 @@ class Node
     {
         this->value = 0;
         this->connected_nodes.clear();
+    };
+};
+
+class MatrixAdj
+{
+  public:
+    ushort **matrix;
+    ushort *size;
+    MatrixAdj(vector<Node *> nodes)
+    {
+        this->size = new ushort;
+        *this->size = nodes.size();
+        this->matrix = new ushort *[*size];
+        for (ushort i = 0; i < *size; i++)
+            this->matrix[i] = new ushort[*size];
+
+        for (Node *n : nodes)
+        {
+            matrix[n->number][n->number] = 0;
+            for (Node *conn_n : n->connected_nodes)
+            {
+                matrix[n->number][conn_n->number] = 1;
+                matrix[conn_n->number][n->number] = 1;
+            }
+        }
+    };
+
+    ~MatrixAdj()
+    {
+        for (ushort count = 0; count < *this->size; count++)
+            delete[] matrix[count];
+    };
+
+    void print()
+    {
+        std::cout << "Print matrix:" << std::endl;
+        for (ushort i = 0; i < *this->size; i++)
+        {
+            for (ushort j = 0; j < *this->size; j++)
+                std::cout << matrix[i][j] << "\t";
+            std::cout << endl;
+        }
+        std::cout << std::flush;
     };
 };
 
@@ -53,6 +97,10 @@ int main(int args, char *argv[])
             cout << "No connected nodes" << endl;
         cout << endl;
     }
+
+    MatrixAdj* matrix = new MatrixAdj(all_nodes);
+    matrix->print();
+    delete matrix;
 
     return 0;
 }
